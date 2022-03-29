8dcf64ed40132b3eb9b1d6ed125a80036a4aa191
Tyler Bulger <tybulger@gmail.com>
2022-03-29 17:56:27 -0400

    worked on the start of ReimbursementController/Repository

--- src/main/java/com/revature/model/Reimbursements.java
+++ src/main/java/com/revature/model/Reimbursements.java
@@ -1,33 +1,47 @@
 package com.revature.model;
 
+import java.sql.Timestamp;
+
 public class Reimbursements {
 
     private Integer reimb_id;
     private Double amount;
-    private String submitted;
-    private String resolved;
+    private Timestamp submitted;
+    private Timestamp resolved;
     private String description;
-    private String receipt;
+    //TODO: Implement receipt if we have the time
+    //private String receipt;
     private Integer payment_id;
     private Integer author_id;
     private Integer resolver_id;
     private Integer status_id;
     private Integer type_id;
 
-    public Reimbursements(Integer reimb_id, Double amount, String submitted, String resolved, String description, String receipt, Integer payment_id, Integer author_id, Integer resolver_id, Integer status_id, Integer type_id) {
+    public Reimbursements(Integer reimb_id, Double amount, Timestamp submitted, Timestamp resolved, String description, Integer payment_id, Integer author_id, Integer resolver_id, Integer status_id, Integer type_id) {
         this.reimb_id = reimb_id;
         this.amount = amount;
         this.submitted = submitted;
         this.resolved = resolved;
         this.description = description;
-        this.receipt = receipt;
+        this.payment_id = payment_id;
+        this.author_id = author_id;
+        this.resolver_id = resolver_id;
+        this.status_id = status_id;
+        this.type_id = type_id;
+
+    }
+
+    public Reimbursements(Double amount, Timestamp submitted, Timestamp resolved, String description, Integer payment_id, Integer author_id, Integer resolver_id, Integer status_id, Integer type_id) {
+        this.amount = amount;
+        this.submitted = submitted;
+        this.resolved = resolved;
+        this.description = description;
         this.payment_id = payment_id;
         this.author_id = author_id;
         this.resolver_id = resolver_id;
         this.status_id = status_id;
         this.type_id = type_id;
     }
-
 
     public Integer getReimb_id() {
         return reimb_id;
@@ -45,19 +59,19 @@ public class Reimbursements {
         this.amount = amount;
     }
 
-    public String getSubmitted() {
+    public Timestamp getSubmitted() {
         return submitted;
     }
 
-    public void setSubmitted(String submitted) {
+    public void setSubmitted(Timestamp submitted) {
         this.submitted = submitted;
     }
 
-    public String getResolved() {
+    public Timestamp getResolved() {
         return resolved;
     }
 
-    public void setResolved(String resolved) {
+    public void setResolved(Timestamp resolved) {
         this.resolved = resolved;
     }
 
@@ -69,13 +83,13 @@ public class Reimbursements {
         this.description = description;
     }
 
-    public String getReceipt() {
-        return receipt;
-    }
-
-    public void setReceipt(String receipt) {
-        this.receipt = receipt;
-    }
+//    public String getReceipt() {
+//        return receipt;
+//    }
+//
+//    public void setReceipt(String receipt) {
+//        this.receipt = receipt;
+//    }
 
     public Integer getPayment_id() {
         return payment_id;
---
+++ src/main/java/com/revature/service/ReimbursementsService.java
@@ -0,0 +1,20 @@
+package com.revature.service;
+
+import com.revature.model.Reimbursements;
+import com.revature.repositories.ReimbursementsRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReimbursementsService {
+    private final Logger logger;
+    private final ReimbursementsRepository reimbursementsRepository;
+
+    public ReimbursementsService(){
+        this.logger = LogManager.getLogger(ReimbursementsService.class);
+        this.reimbursementsRepository = new ReimbursementsRepository();
+    }
+
+    public void create(Reimbursements reimbursements) {
+        reimbursementsRepository.create(reimbursements);
+    }
+}
