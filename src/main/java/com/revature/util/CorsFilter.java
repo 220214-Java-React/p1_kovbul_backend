dceba615466921b724670bc367d855928c80c003
Tyler Bulger <tybulger@gmail.com>
2022-04-01 10:35:27 -0400

    inserted a cors filter

---
+++ src/main/java/com/revature/util/CorsFilter.java
@@ -0,0 +1,36 @@
+package com.revature.util;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class CorsFilter implements Filter {
+
+    private static final Logger log = LogManager.getLogger(CorsFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Adding Access Control Response Headers");
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
